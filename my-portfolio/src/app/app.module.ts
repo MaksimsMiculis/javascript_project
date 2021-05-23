5f0435b1e54b7f60fbe613064681b9eb06df2711
MaksimsMiculis <maksims.miculis@gmail.com>
2021-05-23 13:00:10 -0700

    23.05 update

---
+++ my-portfolio/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PortfolioPageComponent } from './pages/portfolio-page/portfolio-page.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProjectsListComponent } from './components/projects-list/projects-list.component';
+import { SkillsListComponent } from './components/skills-list/skills-list.component';
+import { ToolsListComponent } from './components/tools-list/tools-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PortfolioPageComponent,
+    AboutMeComponent,
+    ContactFormComponent,
+    HeaderComponent,
+    ProjectsListComponent,
+    SkillsListComponent,
+    ToolsListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
