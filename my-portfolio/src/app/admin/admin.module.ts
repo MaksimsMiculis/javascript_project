b87e22472401f7bcb465407d478a941b08e5d337
MaksimsMiculis <maksims.miculis@gmail.com>
2021-05-27 03:28:52 -0700

    27.05 commit

---
+++ my-portfolio/src/app/admin/admin.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminRoutingModule } from './admin-routing.module';
+import { AdminPageComponent } from './pages/admin-page/admin-page.component';
+import { AuthPageComponent } from './pages/auth-page/auth-page.component';
+import { MessagesPageComponent } from './pages/messages-page/messages-page.component';
+import { AdminAboutMeComponent } from './components/admin-about-me/admin-about-me.component';
+import { AdminMessagesComponent } from './components/admin-messages/admin-messages.component';
+import { AdminProjectsListComponent } from './components/admin-projects-list/admin-projects-list.component';
+import { AdminSkillsListComponent } from './components/admin-skills-list/admin-skills-list.component';
+import { AdminToolsListComponent } from './components/admin-tools-list/admin-tools-list.component';
+import { FormModalComponent } from './components/form-modal/form-modal.component';
+import { HeaderComponent } from './components/header/header.component';
+
+
+@NgModule({
+  declarations: [
+    AdminPageComponent,
+    AuthPageComponent,
+    MessagesPageComponent,
+    AdminAboutMeComponent,
+    AdminMessagesComponent,
+    AdminProjectsListComponent,
+    AdminSkillsListComponent,
+    AdminToolsListComponent,
+    FormModalComponent,
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    AdminRoutingModule
+  ]
+})
+export class AdminModule { }
